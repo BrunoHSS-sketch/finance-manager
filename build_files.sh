3635a0fe212f56f81856bc7471fbfcc7fcd0d5af
Bruno H S Santos <129914296+BrunoHSS-sketch@users.noreply.github.com>
2025-10-23 12:57:19 -0300

    Uncomment migration command in build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -8,7 +8,7 @@ python manage.py collectstatic --noinput --clear
 
 # (Opcional, mas recomendado se quiser começar com o esquema limpo na Vercel)
 # Aplicar migrações (cria o schema do banco de dados SQLite na Vercel)
-# python manage.py migrate --noinput
+python manage.py migrate --noinput
 
 # (Opcional: Criar superusuário na Vercel - mais complexo, pode fazer manualmente depois)
 # echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'password')" | python manage.py shell
