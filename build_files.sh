affa92f03dbc3b82c2f698157a38cdc5ee383cec
Bruno H S Santos <129914296+BrunoHSS-sketch@users.noreply.github.com>
2025-10-24 08:50:36 -0300

    Update build_files.sh for migrations and tailwind

--- build_files.sh
+++ build_files.sh
@@ -6,11 +6,12 @@ pip install -r requirements.txt
 # Coletar arquivos estáticos
 python manage.py collectstatic --noinput --clear
 
-# (Opcional, mas recomendado se quiser começar com o esquema limpo na Vercel)
-# Aplicar migrações (cria o schema do banco de dados SQLite na Vercel)
+python manage.py makemigrations
 python manage.py migrate --noinput
+python manage.py tailwind install
+python manage.py collectstatic
+python manage.py tailwind start
 
-# (Opcional: Criar superusuário na Vercel - mais complexo, pode fazer manualmente depois)
-# echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@example.com', 'password')" | python manage.py shell
+
 
 echo "BUILD FINISHED"
