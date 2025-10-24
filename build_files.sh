afad31841e9cad5e6753993cead5bca313c64e4a
Bruno H S Santos <129914296+BrunoHSS-sketch@users.noreply.github.com>
2025-10-24 11:14:35 -0300

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,15 +1,30 @@
 #!/bin/bash
-
+echo "INICIANDO A INSTALAÇÃO DAS DEPENDENCIAS"
 pip install -r requirements.txt
+echo "FINALIZADO A INSTALAÇÃO DAS DEPENDENCIAS"
 
+
+echo "INICIANDO A MIGRATION"
 python manage.py makemigrations
 python manage.py migrate --noinput
+echo "FINALIZADO AS MIGRATION"
 
+
+echo "INICIANDO A INSTALAÇÃO DAS DEPENDENCIAS DO TAILWIND"
 python manage.py tailwind install
 python manage.py tailwind build
+echo "FINALIZADO A INSTALAÇÃO DAS DEPENDENCIAS DO TAILWIND"
 
+
+echo "INICIANDO A CRIAÇÃO DA PASTA STATICFILES"
 mkdir -p /vercel/output/static
 cp -r staticfiles_build/* /vercel/output/static/
 python manage.py collectstatic --noinput --clear
+echo "FINALIZADO A CRIAÇÃO DA PASTA STATICFILES"
+
+echo "Listing contents of /vercel/path0/ (project root in build):"
+ls -la /vercel/path0/
+echo "Listing contents of /vercel/path0/staticfiles_build/ (if created):"
+ls -la /vercel/path0/staticfiles_build/
 
 echo "✅ BUILD FINISHED"
