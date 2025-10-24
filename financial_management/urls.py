57ebe6b59cb4ea1e6a663c50fb32accbf8eca0ad
Bruno H S Santos <129914296+BrunoHSS-sketch@users.noreply.github.com>
2025-10-24 09:57:04 -0300

    Update urls.py

--- financial_management/urls.py
+++ financial_management/urls.py
@@ -16,9 +16,25 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 urlpatterns = [
+    # Redirecionamento para o favicon.ico
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("images/favicon.ico"), permanent=True),
+        name="favicon_ico",
+    ),
+    # Opcional: Redirecionamento para favicon.png (se necessário)
+    path(
+        "favicon.png",
+        RedirectView.as_view(url=staticfiles_storage.url("images/favicon-32x32.png"), permanent=True), # Ou o PNG que preferir
+        name="favicon_png",
+    ),
+
+    # Suas rotas existentes:
     path('admin/', admin.site.urls),
     path('', include('finance.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
+
