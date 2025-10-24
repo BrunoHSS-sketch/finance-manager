dcc4af0200f6512d57de7871e052fcd23c8a53d5
Bruno H S Santos <129914296+BrunoHSS-sketch@users.noreply.github.com>
2025-10-24 11:08:36 -0300

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -8,7 +8,8 @@ python manage.py migrate --noinput
 python manage.py tailwind install
 python manage.py tailwind build
 
-mkdir -p staticfiles_build
+mkdir -p /vercel/output/static
+cp -r staticfiles_build/* /vercel/output/static/
 python manage.py collectstatic --noinput --clear
 
 echo "✅ BUILD FINISHED"
